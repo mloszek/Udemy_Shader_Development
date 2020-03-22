92e4fe57fd1b8ea376e0641518b9e4acbca9cccd
mloszek <miloszloszek@gmail.com>
2020-03-22 13:44:32 +0100

    advance stensils

---
+++ Assets/6.PassesAndBlending/6.5.BlendingTwoImages/DecalOnOff.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DecalOnOff : MonoBehaviour
+{
+    Material mat;
+    bool showDecal = true;
+
+    private void OnMouseDown()
+    {
+        ShowDecal();
+    }
+
+    private void Start()
+    {
+        mat = GetComponent<Renderer>().sharedMaterial;
+        ShowDecal();
+    }
+
+    private void ShowDecal()
+    {
+        showDecal = !showDecal;
+
+        if (showDecal)
+            mat.SetFloat("_ShowDecal", 1);
+        else
+            mat.SetFloat("_ShowDecal", 0);
+    }
+}
